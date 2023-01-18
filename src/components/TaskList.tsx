84cc56eec82f5c390ecbecfa545cb0ace3af1c8f
Mário Santos <mariodev7@gmail.com>
2023-01-18 20:04:24 -0300

    show numbers of tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,9 +1,9 @@
-import { Flex } from "@chakra-ui/react";
+import { Badge, Flex, Text, VStack } from "@chakra-ui/react";
 import { useTask } from "~/context/TaskContext";
 import { Task } from "./Task";
 
 export const TaskList: React.FC = () => {
-  const { tasks } = useTask();
+  const { tasks, totalDoneTasks, totalTasks } = useTask();
 
   return (
     <Flex
@@ -14,9 +14,34 @@ export const TaskList: React.FC = () => {
       mx="auto"
       mt={8}
     >
-      {tasks.map(({ id, title, done }) => (
-        <Task key={id} id={id} title={title} done={done} />
-      ))}
+      <Flex
+        flexDir={["column", "row"]}
+        justifyContent={["flex-start", "space-between"]}
+      >
+        <Flex alignItems="center" gap={2}>
+          <Text color="brand.blue" fontWeight="bold">
+            Tarefas criadas
+          </Text>
+          <Badge rounded="full" px={2} bg="gray.400" color="gray.200">
+            {totalTasks}
+          </Badge>
+        </Flex>
+
+        <Flex alignItems="center" gap={2}>
+          <Text color="brand.purple" fontWeight="bold">
+            Concluídas
+          </Text>
+          <Badge rounded="full" px={2} bg="gray.400" color="gray.200">
+            {totalDoneTasks} de {totalTasks}
+          </Badge>
+        </Flex>
+      </Flex>
+
+      <VStack mt={4} spacing="2">
+        {tasks.map(({ id, title, done }) => (
+          <Task key={id} id={id} title={title} done={done} />
+        ))}
+      </VStack>
     </Flex>
   );
 };
