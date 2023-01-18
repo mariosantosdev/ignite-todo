f42b25b537989dd89b0e4df01cc78e5dbd2eeed9
Mário Santos <mariodev7@gmail.com>
2023-01-18 18:34:12 -0300

    create vite project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Container, Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Container>
+      <Heading textAlign="center">Welcome Vite!</Heading>
+    </Container>
+  );
+}
+
+export default App;
